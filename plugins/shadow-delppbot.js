09e625cba06f4f612719792f5fb5e67b32f0fbb9
the-27 <blackoficial2025@gmail.com>
2025-08-17 07:24:15 -0500

    𝐝𝐯.𝐒𝐡𝐚𝐝𝐨𝐰 💨

--- plugins/shadow-delppbot.js
+++ plugins/shadow-delppbot.js
@@ -1,16 +1,28 @@
-let handler = async (m, { conn, usedPrefix, command, isOwner }) => {
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, isOwner }) => {
   try {
-    await conn.updateProfilePicture(conn.user.jid, Buffer.alloc(0));
-    await conn.reply(m.chat, '✅ Foto de perfil eliminada con éxito.', m, fake);
+
+    if (global.conn.user.jid !== conn.user.jid) {
+      return conn.reply(m.chat, '⚠️ Solo el *Bot Principal* puede usar este comando.', m, rcanal)
+    }
+
+    const transparentImg = Buffer.from(
+      'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNgYAAAAAMAASsJTYQAAAAASUVORK5CYII=',
+      'base64'
+    )
+
+    await conn.updateProfilePicture(conn.user.jid, transparentImg)
+    await conn.reply(m.chat, '✅ Foto de perfil del bot eliminada con éxito.', m, rcanal)
   } catch (e) {
-    console.error(e);
-    await conn.reply(m.chat, '❌ Ocurrió un error al eliminar la foto de perfil.', m);
+    console.error(e)
+    await conn.reply(m.chat, '*❌ Ocurrió un error al intentar eliminar la foto de perfil.', m)
   }
-};
+}
 
-handler.help = ['delppbot'];
-handler.tags = ['owner'];
-handler.command = ['delppbot', 'removeppbot', 'deleteppbot'];
-handler.owner = true;
+handler.help = ['delppbot']
+handler.tags = ['owner']
+handler.command = ['delppbot', 'removeppbot', 'deleteppbot']
+handler.owner = true
 
-export default handler;
+export default handler
