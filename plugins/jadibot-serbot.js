38a5642f5bc2d33c4b73455f66c7f8f2695026be
the-27 <blackoficial2025@gmail.com>
2025-08-17 07:15:59 -0500

    𝐝𝐯.𝐒𝐡𝐚𝐝𝐨𝐰 💨

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -161,37 +161,8 @@ if (qr && mcode) {
 let secret = await sock.requestPairingCode((m.sender.split`@`[0]))
 secret = secret.match(/.{1,4}/g)?.join("-")
 
-const msg = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-  interactiveMessage: {
-    body: { text: rtx2 }, 
-    footer: { text: `${dev}` },
-    header: {
-      hasMediaAttachment: false 
-    },
-    nativeFlowMessage: {
-      buttons: [
-        {
-          name: 'cta_copy',
-          buttonParamsJson: JSON.stringify({
-            display_text: 'Copiar Codigo',
-            copy_code: secret
-          })
-        }
-      ]
-    }
-  }
-}), { quoted: m })
-
-txtCode = await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-
-} else {
-txtCode = await conn.sendButton(m.chat, rtx2.trim(), wm, null, [], secret, null, m) 
-},
-
-
-
-//txtCode = await conn.sendMessage(m.chat, {text : rtx2}, { quoted: m })
-//codeBot = await conn.reply(m.chat, `\`${secret}\``, m, fake);
+txtCode = await conn.sendMessage(m.chat, {text : rtx2}, { quoted: m })
+codeBot = await conn.reply(m.chat, `\`${secret}\``, m, fake);
 
 console.log(secret)
 }
