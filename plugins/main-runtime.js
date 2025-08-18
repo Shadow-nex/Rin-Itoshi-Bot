0c63d02e16ef8f1f6433b7f8c1875e79e4f1d089
the-27 <blackoficial2025@gmail.com>
2025-08-18 18:09:11 -0500

    𝐝𝐯.𝐒𝐡𝐚𝐝𝐨𝐰 💨

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -1,40 +1,23 @@
-import fetch from 'node-fetch'
+let handler = async (m, { usedPrefix, command }) => {
+let uptime = await process.uptime()
+let runtime = `${global.botname}
 
-let handler = async (m, { conn }) => {
-  let uptime = await process.uptime()
-
-  let infoNet = {}
-  try {
-    let res = await fetch('https://ipapi.co/json/')
-    infoNet = await res.json()
-  } catch (e) {
-    infoNet = { ip: "No disponible", country_name: "Desconocido" }
-  }
-
-  let os = process.platform
-  let nodeVer = process.version
-
-  let runtime = `𓆩⚝𖥔𓂃 𝐔𝐥𝐭𝐫𝐚 𝐒𝐭𝐚𝐭𝐮𝐬 𓂃𖥔⚝𓆪
-
-╭━━━〔 ✦ 𝑹𝒊𝒏 𝑰𝒕𝒐𝒔𝒉𝒊 𝑩𝒐𝒕 ✦ 〕━━⬣
-┃ ✦꒰⏳꒱ 𝐓𝐢𝐞𝐦𝐩𝐨 activo: *${rTime(uptime)}*
-┃ ✦꒰📡꒱ Estado: 🟢 *En línea*
-┃ ✦꒰🤖꒱ Bot: *${bot}*
-┃ ✦꒰💻꒱ Sistema: *Óptimo y estable*
-┃ ✦꒰⚙️꒱ Node.js: *${nodeVer}*
-┃ ✦꒰🖥️꒱ Plataforma: *${os}*
-┃ ✦꒰🌐꒱ IP Pública: *${infoNet.ip}*
-┃ ✦꒰🏳️꒱ Ubicación: *${infoNet.country_name || 'Desconocido'}*
-╰━━━━━━━━━━━━━━━━━━⬣`
-
-  conn.reply(m.chat, runtime, m, fake)
+✰ Tiempo activo: ${rTime(uptime)}`
+conn.reply(m.chat, runtime, m)
 }
-
 handler.help = ['runtime']
 handler.tags = ['main']
 handler.command = ['runtime', 'uptime']
 
 export default handler
+
+const dd = new Date(new Date + 3600000);
+const time = dd.toLocaleString('en-US', { 
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: true 
+    });
 
 function rTime(seconds) {
   seconds = Number(seconds);
@@ -42,9 +25,9 @@ function rTime(seconds) {
   var h = Math.floor((seconds % (3600 * 24)) / 3600);
   var m = Math.floor((seconds % 3600) / 60);
   var s = Math.floor(seconds % 60);
-  var dDisplay = d > 0 ? d + (d == 1 ? " día, " : " Días, ") : "";
+  var dDisplay = d > 0 ? d + (d == 1 ? " dia, " : " Dias, ") : "";
   var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " Horas, ") : "";
   var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " Minutos, ") : "";
   var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " Segundos") : "";
   return dDisplay + hDisplay + mDisplay + sDisplay;
-}
+};
