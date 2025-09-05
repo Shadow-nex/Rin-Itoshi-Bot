5cbbf2e0d5c4a0b46bc4d4640e1b2a184df97226
𝐝𝐯.𝐒𝐡𝐚𝐝𝐨𝐰 <blackoficial2025@gmail.com>
2025-09-05 17:09:30 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -474,7 +474,7 @@ await plugin.after.call(this, m, extra)
 console.error(e)
 }}
 if (m.coin)
-conn.reply(m.chat, `❮✦❯ Utilizaste ${+m.coin} ${moneda}`, m)
+conn.reply(m.chat, `*❮✦❯ Utilizaste ${+m.coin} ${moneda}*`, m)
 }
 break
 }}
