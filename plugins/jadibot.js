b33d90265815afa5be9bb27883cef6313ff7a8b8
the-27 <blackoficial2025@gmail.com>
2025-09-07 18:17:36 -0500

    ☆ 𝚂𝙷𝙰𝙳𝙾𝚆-𝚇𝚈𝚉🍧

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -1,18 +1,51 @@
+import axios from 'axios'
 import ws from 'ws';
+import fs from 'fs'
 
 let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
   if (!globalThis.db.data.settings[conn.user.jid].jadibotmd) 
-    return conn.reply(m.chat, `☁️ El comando *${command}* está desactivado temporalmente.`, m, fake);
+    return conn.reply(m.chat, `☁️ El comando *${command}* está desactivado temporalmente.`, m);
 
   const channelRD = { 
     id: '120363401008003732@newsletter', 
     name: '⚽𐚁 ֹ ִ Rin Itoshi - Official ୧ ֹ ִ⚽ᩚ꤬ᰍ'
   };
 
+  const getThumbnail = async () => {
+    const res = await axios.get("https://d.uguu.se/decJwDTx.jpg", { responseType: "arraybuffer" })
+    return Buffer.from(res.data, "binary")
+  }
+
+  const thumbnail = await getThumbnail()
+
+  const shadow_xyz = {
+    key: {
+      fromMe: false,
+      participant: "0@s.whatsapp.net",
+      remoteJid: "status@broadcast"
+    },
+    message: {
+      productMessage: {
+        product: {
+          productImage: {
+            mimetype: "image/jpeg",
+            jpegThumbnail: thumbnail
+          },
+          title: "☆ 🍧 𝐒𝐔𝐁𝐁𝐎𝐓𝐒 | 𝐎𝐍𝐋𝐈𝐍𝐄 🍧☆",
+          description: dev,
+          currencyCode: "USD",
+          priceAmount1000: 5000,
+          retailerId: "SubBots",
+          productImageCount: 1
+        },
+        businessOwnerJid: "13135550002@s.whatsapp.net"
+      }
+    }
+  }
+
   const connsActivas = global.conns.filter(conn =>
     conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED
   );
-  
 
   const _muptime = process.uptime() * 1000;
   const uptime = clockString(_muptime);
@@ -46,7 +79,6 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
   const maxSubbots = 10;
   const disponibles = maxSubbots - total;
 
-  //const readMore = String.fromCharCode(8206).repeat(4001)
   const lista = subbotsUnicos.map((bot, i) => {
     return `╭━═┅═━──────────◈
 │ャ □ ➛ 𝚂𝙾𝙲𝙺𝙴𝚃 ~ #${i + 1} ﾟ｡◌
@@ -62,7 +94,6 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
  ﹍﹍⏜͡︵(╲  ִ ⚽ ִ  ╱)︵͡⏜﹍﹍
 ┃⋆⸼˟꘏⪩ \`ᴘᴀɴᴇʟ ᴅᴇ sᴜʙ ʙᴏᴛs\` ⪨꙳ʾ˒˓ʿʾ˒˓ʿ┃
 ┗━━━━━━━━━━━━━━━━━━━━━┛
-࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚࿙࿚
 
 ╔╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╗
 ╏✎ *°ʀᴜɴᴛɪᴍᴇ:* _${uptime}_
@@ -70,35 +101,37 @@ let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
 ╏✎ *°sᴏᴄᴋᴇᴛs ᴄᴏɴᴇᴄᴛᴀᴅᴏs:* _${total}_
 ╚╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╝
 
-
   🌱 \`\`\`LIST DE SUBS CONECTADOS\`\`\`🔋
 
 ${lista || '🌙 No hay Sub-Bots conectados por ahora verifique mas tarde.'}
 
 > ${club}`;
 
-await conn.sendMessage(m.chat, {
-  text: textoSubbots,
-  contextInfo: {
-    mentionedJid: [m.sender],
-    isForwarded: true,
-    forwardedNewsletterMessageInfo: {
-      newsletterJid: channelRD.id,
-      serverMessageId: 99999,
-      newsletterName: channelRD.name
+  await conn.sendMessage(
+    m.chat,
+    {
+      image: { url: 'https://files.catbox.moe/z1zfg6.jpg' },
+      caption: textoSubbots.trim(),
+      contextInfo: {
+        mentionedJid: [m.sender],
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: channelRD.id,
+          serverMessageId: 99999,
+          newsletterName: channelRD.name
+        },
+        externalAdReply: {
+          title: `🍧 𝑺𝑶𝑪𝑲𝑬𝑻𝑺 𝑨𝑪𝑻𝑰𝑽𝑶𝑺 🍧`,
+          body: `💔 𝙲𝙾𝙽𝙴𝙲𝚃𝙰𝙳𝙾𝚂: ${total}/${maxSubbots}`,
+          thumbnailUrl: 'https://tinyurl.com/28st4n83',
+          sourceUrl: redes,
+          mediaType: 1,
+          renderLargerThumbnail: true
+        }
+      }
     },
-    externalAdReply: {
-      title: '🌀 ѕσ¢ƙєтѕ αcтιvσѕ 🌂',
-      body: `🍂 Conectados: ${total}/${maxSubbots}`,
-      thumbnailUrl: 'https://tinyurl.com/28st4n83',
-      mediaUrl: 'https://tinyurl.com/28st4n83',
-      sourceUrl: redes,
-      mediaType: 1,
-      renderLargerThumbnail: true,
-      showAdAttribution: true
-    }
-  }
-}, { quoted: m });
+    { quoted: shadow_xyz }
+  )
 };
 
 handler.command = ['sockets', 'bots', 'socket'];
