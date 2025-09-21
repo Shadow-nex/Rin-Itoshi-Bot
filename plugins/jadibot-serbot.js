427aa4a504eb684ff5f2a87646984af7df9f28b9
the-27 <blackoficial2025@gmail.com>
2025-09-21 17:54:23 -0500

    🎋 𝙎𝙝𝙖𝙙𝙤𝙬𝘾𝙤𝙧𝙚 🌿

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -208,7 +208,7 @@ codeBot = await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id }
 
 //txtCode = await conn.sendMessage(m.chat, {text : rtx2}, { quoted: m })
 
-txtCode = await conn.sendFile(m.chat, banner, 'code.jpg', rtx2, m, null, rcanal)
+txtCode = await conn.sendMessage(m.chat, { text: rtx2, video: { url: "https://files.catbox.moe/z09oz9.mp4" }, gifPlayback: true, ...rcanal }, { quoted: m })
 
 codeBot = await conn.reply(m.chat, `*${secret}*`, m, fake);
 
