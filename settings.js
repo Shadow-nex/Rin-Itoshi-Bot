b2ce0ff210149831fcb335d6d0b0795427475f33
DV-ux-alt <lopezacaro09@gmail.com>
2025-09-22 06:04:38 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -102,6 +102,6 @@ global.moment = moment
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
+  console.log(chalk.redBright("Update 'settings.js'"))
   import(`${file}?update=${Date.now()}`)
 })
