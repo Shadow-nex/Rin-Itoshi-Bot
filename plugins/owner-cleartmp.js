e1374c798ec40824d3c23c97ca1e3af52451947f
the-27 <blackoficial2025@gmail.com>
2025-10-03 14:28:09 -0500

    🎋 𝙎𝙝𝙖𝙙𝙤𝙬𝘾𝙤𝙧𝙚 🌿

--- plugins/owner-cleartmp.js
+++ plugins/owner-cleartmp.js
@@ -1,24 +1,27 @@
 import { tmpdir } from 'os'
 import path, { join } from 'path'
-import {
-readdirSync,
-statSync,
-unlinkSync,
-existsSync,
-readFileSync,
-watch
-} from 'fs'
-let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => { 
+import { readdirSync, statSync, unlinkSync, existsSync } from 'fs'
 
-conn.reply(m.chat, `${emoji} Realizado, ya se ha eliminado los archivos de la carpeta tmp`, m)
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => { 
+  const emoji = '✅'
+  conn.reply(m.chat, `${emoji} Realizado, ya se ha eliminado los archivos de la carpeta tmp`, m)
 
-const tmp = [tmpdir(), join(__dirname, '../tmp')]
-const filename = []
-tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
-return filename.map(file => {
-const stats = statSync(file)
-unlinkSync(file)
-})}
+  const tmp = [tmpdir(), join(__dirname, '../tmp')]
+  const filename = []
+
+  tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
+
+  filename.forEach(file => {
+    try {
+      if (existsSync(file)) {
+        const stats = statSync(file)
+        if (stats.isFile()) unlinkSync(file)
+      }
+    } catch (e) {
+      console.error(`No se pudo borrar ${file}:`, e.message)
+    }
+  })
+}
 
 handler.help = ['cleartmp']
 handler.tags = ['owner']
