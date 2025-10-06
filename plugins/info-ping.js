ebb8d3deead5b1f4d7f988a22e489ce7f9b2c131
the-27 <blackoficial2025@gmail.com>
2025-10-06 13:07:18 -0500

    Update

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,31 +1,32 @@
 import speed from 'performance-now'
 import { exec } from 'child_process'
 import moment from 'moment-timezone'
+import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-  let timestamp = speed();
-  let latensi = speed() - timestamp;
+  let timestamp = speed()
+  let latensi = speed() - timestamp
 
-  const start = new Date().getTime();
-  const { key } = await conn.sendMessage(m.chat, { text: `*🍂 Calculando ping...*` }, { quoted: m });
-  const end = new Date().getTime();
-  const latency = end - start;
+  const start = new Date().getTime()
+  await conn.sendMessage(m.chat, { text: `*🍂 Calculando ping...*` }, { quoted: m })
+  const end = new Date().getTime()
+  const latency = end - start
 
-  const uptime = process.uptime();
-  const hours = Math.floor(uptime / 3600);
-  const minutes = Math.floor((uptime % 3600) / 60);
-  const secondsUp = Math.floor(uptime % 60);
-  const uptimeFormatted = `${hours}h ${minutes}m ${secondsUp}s`;
+  const uptime = process.uptime()
+  const hours = Math.floor(uptime / 3600)
+  const minutes = Math.floor((uptime % 3600) / 60)
+  const secondsUp = Math.floor(uptime % 60)
+  const uptimeFormatted = `${hours}h ${minutes}m ${secondsUp}s`
 
-  const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
+  const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)
+  const fechaHora = moment().tz('America/Lima').format('YYYY/MM/DD, h:mm A')
 
-  const fechaHora = moment().tz('America/Lima').format('YYYY/MM/DD, h:mm A');
+  const thumbBuffer = Buffer.from(await (await fetch('https://n.uguu.se/vqJnHBPm.jpg')).arrayBuffer())
 
   exec(`neofetch --stdout`, async (error, stdout) => {
-    let child = stdout.toString("utf-8");
-    let sysInfo = child.replace(/Memory:/, "Ram:");
+    let sysInfo = stdout.toString("utf-8").replace(/Memory:/, "Ram:")
 
-    let response =
+    let response = 
 `╭━━━〔 🍄 𝙎𝙩𝙖𝙩𝙪𝙨 𝙋𝙞𝙣𝙜 🪴 〕━━⬣
 │ 🌷 *\`Ping:\`* ${latency} ms
 │ 🍁 *\`Latencia:\`* ${latensi.toFixed(4)} ms
@@ -33,9 +34,8 @@ let handler = async (m, { conn }) => {
 │ 🍧 *\`Uptime:\`* ${uptimeFormatted}
 │ ⏰ *\`Fecha/Hora:\`* ${fechaHora}
 ╰━━━〔 🪷 𝙍𝙞𝙣 𝙄𝙩𝙤𝙨𝙝𝙞 〕━━⬣
-\`\`\`
-${sysInfo.trim()}
-\`\`\``;
+\`\`\`${sysInfo.trim()}\`\`\`
+> кαиєкι вσт ν3 | 𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝚂𝙷𝙰𝙳𝙾𝚆-𝚇𝚈𝚉`
 
     await conn.sendMessage(m.chat, {
       text: response,
@@ -44,14 +44,14 @@ ${sysInfo.trim()}
         externalAdReply: {
           title: '🍄 Rɪɴ Iᴛᴏsʜɪ ᴍᴅ 🌹 | 🪾 ʙʏ ᴅᴠ.sʜᴀᴅᴏᴡ 🪴',
           body: club,
-          thumbnailUrl: await (await fetch('https://n.uguu.se/vqJnHBPm.jpg')).buffer(),
+          thumbnail: thumbBuffer,
           sourceUrl: redes,
           mediaType: 1,
           renderLargerThumbnail: true
         }
       }
-    }, { quoted: m });
-  });
+    }, { quoted: fkontak })
+  })
 }
 
 handler.help = ['ping', 'p']
