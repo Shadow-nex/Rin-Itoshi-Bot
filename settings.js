85025139a00c0c04f230bab1c97e1fd3f7190bda
DV-ux-alt <lopezacaro09@gmail.com>
2025-10-19 06:15:24 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -82,9 +82,9 @@ global.correo = 'blackoficial2025@gmail.com'
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
-ch1: '120363401008003732@newsletter',
-ch2: "120363401008003732@newsletter",
-ch3: "120363401008003732@newsletter"
+ch1: "120363422142340004@newsletter",
+ch2: "120363422142340004@newsletter",
+ch3: "120363422142340004@newsletter"
 }
 global.multiplier = 60
 
